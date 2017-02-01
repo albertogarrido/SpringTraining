4cb46545c84549514ee21a9041e4ce269d60e039
Alberto Garrido <alberto.garrido-trullenque@flixbus.com>
2017-02-01 18:59:16 +0100

    Add RESTful guide example

---
+++ SpringRestGuide/README.md
@@ -0,0 +1,6 @@
+# Building a RESTful Web Service
+## Spring Framework guides
+
+https://spring.io/guides/gs/rest-service/
+
+./gradlew bootRun
---
+++ SpringRestGuide/src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by agarrido on 01/02/17.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
