39dc37095f30655b7136c921d976805275b9d1a7
Alberto Garrido <alberto.garrido-trullenque@flixbus.com>
2017-02-01 19:57:28 +0100

    fix  untracked files, again

--- SpringRestGuide/src/main/java/hello/Greeting.java
+++ SpringRestGuide/src/main/java/hello/Greeting.java
@@ -5,11 +5,13 @@ import com.google.auto.value.AutoValue;
 @AutoValue
 public abstract class Greeting {
 
-    public static Greeting create(long id, String content) {
-        return new AutoValue_Greeting(id, content);
+    public static Greeting create(long id, String content, String date) {
+        return new AutoValue_Greeting(id, content, date);
     }
 
     public abstract long id();
 
     public abstract String content();
+
+    public abstract String date();
 }
